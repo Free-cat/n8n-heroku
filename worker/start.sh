a3c29ce7ab5dc17363f2770e1a6e9a2f0793b190
Artem Melnikov <freecats1997@gmail.com>
2024-02-23 15:19:14 +0100

    Update start.sh

--- worker/start.sh
+++ worker/start.sh
@@ -59,10 +59,10 @@ else
 fi
 
 # parse REDIS_URL if present
-if [ "$REDIS_URL" ]
+if [ "$REDISCLOUD_URL" ]
 then 
 	echo "redis config detected"
-	PREFIX="N8N_REDIS_" parse_url "$REDIS_URL"
+	PREFIX="N8N_REDIS_" parse_url "$REDISCLOUD_URL"
 	# Separate host and port    
 	N8N_REDIS_HOST="$(echo $N8N_REDIS_HOSTPORT | sed -e 's,:.*,,g')"
 	N8N_REDIS_PORT="$(echo $N8N_REDIS_HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
